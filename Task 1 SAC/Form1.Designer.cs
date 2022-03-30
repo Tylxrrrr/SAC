53121f32cfc7545466b7ca562e5d2acbfcd5e72c
Tylxrrrr <81595902+Tylxrrrr@users.noreply.github.com>
2022-03-30 15:04:47 +1100

    Add files via upload

---
+++ Task 1 SAC/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Task_1_SAC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ageNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.purchasePriceNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.endQuoteButton = new System.Windows.Forms.Button();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.hintLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ageNumUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.purchasePriceNumUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How old is the textbook?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "How much did you pay for it?";
+            // 
+            // ageNumUpDown
+            // 
+            this.ageNumUpDown.Location = new System.Drawing.Point(46, 95);
+            this.ageNumUpDown.Name = "ageNumUpDown";
+            this.ageNumUpDown.Size = new System.Drawing.Size(250, 29);
+            this.ageNumUpDown.TabIndex = 2;
+            // 
+            // purchasePriceNumUpDown
+            // 
+            this.purchasePriceNumUpDown.Location = new System.Drawing.Point(46, 216);
+            this.purchasePriceNumUpDown.Name = "purchasePriceNumUpDown";
+            this.purchasePriceNumUpDown.Size = new System.Drawing.Size(250, 29);
+            this.purchasePriceNumUpDown.TabIndex = 3;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(46, 271);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(150, 50);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // endQuoteButton
+            // 
+            this.endQuoteButton.Location = new System.Drawing.Point(359, 271);
+            this.endQuoteButton.Name = "endQuoteButton";
+            this.endQuoteButton.Size = new System.Drawing.Size(150, 50);
+            this.endQuoteButton.TabIndex = 6;
+            this.endQuoteButton.Text = "End quote";
+            this.endQuoteButton.UseVisualStyleBackColor = true;
+            this.endQuoteButton.Click += new System.EventHandler(this.endQuoteButton_Click);
+            // 
+            // valueLabel
+            // 
+            this.valueLabel.AutoSize = true;
+            this.valueLabel.Location = new System.Drawing.Point(41, 535);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(288, 25);
+            this.valueLabel.TabIndex = 7;
+            this.valueLabel.Text = "";
+            // 
+            // hintLabel
+            // 
+            this.hintLabel.AutoSize = true;
+            this.hintLabel.Location = new System.Drawing.Point(46, 346);
+            this.hintLabel.Name = "hintLabel";
+            this.hintLabel.Size = new System.Drawing.Size(64, 25);
+            this.hintLabel.TabIndex = 8;
+            this.hintLabel.Text = "Enter book(s) details for a quote";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 732);
+            this.Controls.Add(this.hintLabel);
+            this.Controls.Add(this.valueLabel);
+            this.Controls.Add(this.endQuoteButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.purchasePriceNumUpDown);
+            this.Controls.Add(this.ageNumUpDown);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ageNumUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.purchasePriceNumUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown ageNumUpDown;
+        private System.Windows.Forms.NumericUpDown purchasePriceNumUpDown;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button endQuoteButton;
+        private System.Windows.Forms.Label valueLabel;
+        private System.Windows.Forms.Label hintLabel;
+    }
+}
+
---
+++ Task 1 SAC/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_1_SAC
+{
+    public partial class Form1 : Form
+    {
+        private double totalValue = 0f;
+        private double bookValue = 0f;
+        private int bookCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // Check for zero purchase price
+            if (purchasePriceNumUpDown.Value <= 0)
+            {
+                hintLabel.Text = "Please enter a purchase price greater than zero";
+                return;
+            } 
+            else
+            {
+            // Calculate the total value of each book
+            bookValue = calculateCurrentValue(Convert.ToDouble(purchasePriceNumUpDown.Value), Convert.ToDouble(ageNumUpDown.Value));
+            totalValue = totalValue + bookValue;
+            
+            // Increment the count for books quoted
+            bookCount++;
+
+            // Display the calculated individual and total value of all books and the number of books 
+            valueLabel.Text = String.Format("It is worth ${0}\nThe collection so far is worth ${1} ({2} book(s))", bookValue, totalValue, bookCount);
+            }
+        }
+
+
+        private double calculateCurrentValue(double purchaseValue, double age)
+        {
+            // Calculate the depreciation value of each book
+            double depreciation = purchaseValue * 0.2 * (Convert.ToInt64(age));
+            if (depreciation > purchaseValue)
+            {
+                return 0f;
+            }
+            return purchaseValue - depreciation;
+        }
+
+        private void endQuoteButton_Click(object sender, EventArgs e)
+        {
+            // Reset all values to zero when the end quote button is pressed
+            ageNumUpDown.Value = 0;
+            purchasePriceNumUpDown.Value = 0;
+            totalValue = 0f;
+            bookValue = 0f;
+            bookCount = 0;
+
+            // Reset all labels 
+            valueLabel.Text = "";
+            hintLabel.Text = "Enter book(s) details for a quote";
+        }
+    }
+}
